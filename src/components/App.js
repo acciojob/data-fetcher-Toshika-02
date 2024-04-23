d98ebde3513dae12fe45fd0839e3acfd48eb760a
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-23 21:33:07 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [data, setData] = useState([])
-  const [isLoading, setLoading] = useState(true)
+  const [data, setdata] = useState([]);
+
   useEffect(() => {
-    fetch('https://dummyjson.com/products')
-      .then(data => data.json())
-      .then(data => {
-        setData(data)
-        setLoading(false)
-      })
-  })
-  if (isLoading) return <h1>Loading...</h1>
-  if(data.message) return <h1>An error occurred: {data.message}</h1>
+    function fetchData() {
+      fetch("https://dummyjson.com/products")
+        .then((res) => (res.json()))
+        .then((res) => { setdata(res) })
+        .catch((err) => { console.log(err); setdata(`An error occurred: ${err}`) })
+    }
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading product data...</p>;
+  }
+
   return (
-    <div>
+    <div className="app">
       <h1>Data Fetched from API</h1>
-      <pre>
-        {/* Do not remove the main div */}
-        {JSON.stringify(data,null,2)}
-      </pre>
+      <pre className="data">{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
